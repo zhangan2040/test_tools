421874da77dbabd82589f9f8281e59fe21bbb5e0
zhangan2040 <anzhang28@163.com>
2019-08-16 09:48:55 +0800

    MOD|兼容时间戳毫秒级输入

--- pytools.py
+++ pytools.py
@@ -11,44 +11,6 @@ import time
 import base64
 
 
-class DateTrans():
-    def __init__(self):
-        self.dt = tk.Tk()
-        self.dt.title('Time-Stamp tools by Python(zhangan)')
-        self.date = tk.StringVar()
-        self.date.set(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-        self.stamp = tk.StringVar()
-        self.stamp.set(time.time())
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.date2stamp = tk.LabelFrame(self.dt, text='时间戳转换工具：', height=10, width=200)
-        self.date2stamp.grid()
-        self.lf_date_label = tk.Label(self.date2stamp, text='日期：')
-        self.lf_date_label.grid(row=0, column=0, ipadx=10, ipady=5)
-        self.lf_date_label = tk.Label(self.date2stamp, text='时间戳：')
-        self.lf_date_label.grid(row=0, column=2, ipadx=10, ipady=5)
-        # 用于显示日期
-        self.lf_date_entry = tk.Entry(self.date2stamp, textvariable=self.date)
-        self.lf_date_entry.grid(row=0, column=1, sticky=tk.W, padx=10)
-        # 用于显示时间戳
-        self.lf_stamp_entry = tk.Entry(self.date2stamp, textvariable=self.stamp)
-        self.lf_stamp_entry.grid(row=0, column=3, sticky=tk.W, padx=10)
-        # 添加转换按钮
-        self.s2d_button = tk.Button(self.date2stamp, text='<<', command=self.stamp_date)
-        self.s2d_button.grid(row=1, column=1, ipadx=10)
-        self.d2s_button = tk.Button(self.date2stamp,text='>>', command=self.date_stamp)
-        self.d2s_button.grid(row=1, column = 3, ipadx=10)
-
-    def date_stamp(self):
-        cur_time = time.strptime(self.lf_date_entry.get(), '%Y-%m-%d %H:%M:%S')
-        self.stamp.set(time.mktime(cur_time))
-
-    def stamp_date(self):
-        cur_time = time.localtime(float(self.lf_stamp_entry.get()))
-        self.date.set(time.strftime('%Y-%m-%d %H:%M:%S', cur_time))
-
-
 class SoulTools(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
@@ -120,7 +82,7 @@ class SoulTools(tk.Tk):
         self.stamp_var.set(int(time.mktime(cur_time)))
 
     def stamp_date(self):
-        cur_time = time.localtime(int(self.stamp_entry.get()))
+        cur_time = time.localtime(int(self.stamp_entry.get()[:10]))
         self.date_var.set(time.strftime('%Y-%m-%d %H:%M:%S', cur_time))
 
     def bs_codec(self):
